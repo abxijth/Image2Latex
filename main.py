f81d1e77c3dc8c758659ba783643a73d84985ae5
abxijth <abhijithrpillai231@gmail.com>
2025-10-29 13:33:02 +0530

    Initial Test Run (Sample Html + Flask (backend)) - (error: forgot to define 'latex_text on GET)

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from flask import Flask, request, render_template
 from PIL import Image
 from pix2tex.cli import LatexOCR
+import io #to convert the image to acceptable format
 
-img = Image.open('image.png')
-model = LatexOCR()
-print(model(img))
+app = Flask(__name__)
+model = LatexOCR() #main model that converts image to Latex
+
+#Connect The Frontend
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        uploaded_image = request.files["image"] #served from frontend
+        image = Image.open(io.BytesIO(uploaded_image.read()))
+        latex_text = model(image)
+    
+
+
+
+    return render_template("index.html", result=latex_text)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
